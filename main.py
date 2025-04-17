5cfe02d2dd13e9a5cf2b2320a1bbfc3db3a6c46e
marweenie <133703779+marweenie@users.noreply.github.com>
2025-04-16 22:18:21 -0400

    Main Screen Window Setup

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #Main file
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 600, 400
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Snake Game")
+
+running = True
+while running:
+    screen.fill((0,0,0)) #black
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+pygame.quit()
