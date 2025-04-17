a223f06b69745fa9430dc932f4b47b2421014097
msepse <114550628+msepse@users.noreply.github.com>
2025-04-16 22:49:04 -0400

    Got Namaa's snake to appear

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ WIDTH, HEIGHT = 600, 400
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Snake Game")
 
+fps = pygame.time.Clock()
+
+snake_speed = 15
+
 snake_position = [100, 50]
 snake_body = [[100, 50]]
 
 running = True
 while running:
-    screen.fill((0,0,0)) #black
-
+# handling key events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill((0,0,0)) #black
+
+    for pos in snake_body:
+        pygame.draw.rect(screen, (0, 255, 0),
+                         pygame.Rect(pos[0], pos[1], 10, 10))
 
 
     pygame.display.update()
